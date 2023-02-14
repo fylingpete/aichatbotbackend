452c89b227f5a0d71ed37b60350f6577a57027e0
Waleed Rafi <56467580+Waleed-Rafi@users.noreply.github.com>
2023-02-14 23:53:58 +0500

    updated code

---
+++ index.js
@@ -0,0 +1,68 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+require("dotenv-flow").config();
+
+require("./routes/middlewares/mongo");
+
+const app = express();
+const port = 3080;
+
+// Open AI Configuration
+const configuration = new Configuration({
+  organization: process.env.ORGANIZATION,
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  if (req.originalUrl === "/api/stripe/webhook") {
+    next();
+  } else {
+    bodyParser.json()(req, res, next);
+  }
+});
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+app.set("jwt", "ebeb1a5ada5cf38bfc2b49ed5b3100e0");
+
+app.use("/api", require("./routes/api"));
+
+// Primary Open AI Route-------
+app.post("/", async (req, res) => {
+  let { message, currentModel, temperature } = req.body;
+  currentModel = "text-davinci-003";
+  const response = await openai.createCompletion({
+    model: `${currentModel}`, // "text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature,
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+
+// Get Models Route
+app.get("/models", async (req, res) => {
+  const response = await openai.listEngines();
+  res.json({
+    models: response.data,
+  });
+});
+// ----------------
+// Default Index Page
+app.use(express.static(__dirname + "/build"));
+// Send all other items to index file
+app.get("*", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${process.env.DOMAIN}:${port}`);
+});
