0377dfa75328d67a156d986dc71e87adc73c33d1
Waleed Rafi <56467580+Waleed-Rafi@users.noreply.github.com>
2023-02-15 01:07:02 +0500

    remove auth revert

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ const openai = new OpenAIApi(configuration);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
-// app.use((req, res, next) => {
-//   if (req.originalUrl === "/api/stripe/webhook") {
-//     next();
-//   } else {
-//     bodyParser.json()(req, res, next);
-//   }
-// });
+app.use((req, res, next) => {
+  if (req.originalUrl === "/api/stripe/webhook") {
+    next();
+  } else {
+    bodyParser.json()(req, res, next);
+  }
+});
 // for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true }));
 //form-urlencoded
-// app.set("jwt", "ebeb1a5ada5cf38bfc2b49ed5b3100e0");
+app.set("jwt", "ebeb1a5ada5cf38bfc2b49ed5b3100e0");
 
 app.use("/api", require("./routes/api"));
 
--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ const apiLimiter = rateLimit({
 
 let app = express.Router();
 
-// const authJwt = require("./auth/authJwt");
+const authJwt = require("./auth/authJwt");
 
 // Webhooks and things
 app.use("/stripe", require("./stripe"));
@@ -19,7 +19,7 @@ app.use("/", apiLimiter);
 app.use("/auth", require("./auth"));
 
 // Everything after this requires user authentication
-// app.use('/', authJwt.verifyToken);
+app.use("/", authJwt.verifyToken);
 
 // Already signed up user routes
 app.use("/user", require("./user"));
